009a6145f81e6461d3e49759582c8613170af031
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-03-25 16:34:26 +0900

    Updates

---
+++ precache-manifest.1603a264be756129f0b3bfbdd4351f82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c89d482c2b2bb7c092896320ecd22ba9",
+    "url": "/coffee-menu/index.html"
+  },
+  {
+    "revision": "179f280f7bb7aa9b57b1",
+    "url": "/coffee-menu/static/css/main.312ec82b.chunk.css"
+  },
+  {
+    "revision": "75193bf7845fc927a085",
+    "url": "/coffee-menu/static/js/2.ce6e983b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/coffee-menu/static/js/2.ce6e983b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "179f280f7bb7aa9b57b1",
+    "url": "/coffee-menu/static/js/main.384b28e3.chunk.js"
+  },
+  {
+    "revision": "c4b88bce4b037cab9295",
+    "url": "/coffee-menu/static/js/runtime-main.b5bd9fb9.js"
+  }
+]);
