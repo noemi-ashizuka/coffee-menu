e5f2891db0a5053a37cf4c9b6ba75c5c324bd899
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-03-25 16:18:33 +0900

    added font

--- src/App.js
+++ src/App.js
@@ -33,6 +33,7 @@ class App extends Component {
     
     return (
       <div className="App">
+        <h1>Coffee Menu</h1>
         <SearchBox
           placeholder='search coffees'
           handleChange={ this.handleChange }
